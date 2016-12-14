870eeb61e36d9aeb09573d47a34b8e658f71877f
Douglas Quintanilha <dougquintanilha@gmail.com>
2016-12-14 19:19:15 -0200

    Configurando portas

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var app = require('./config/express');
 
-http.createServer(app).listen(3000, function() {
+app.set('port', (process.env.PORT || 3000));
+
+http.createServer(app).listen(app.get('port'), function() {
 	console.log('Servidor iniciado');
 });
