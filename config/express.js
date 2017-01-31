ac28bff50e2c332d3e9e2072472c54e8073b3d89
Douglas Quintanilha <dougquintanilha@gmail.com>
2017-01-31 14:58:20 -0200

    Better Json formating

--- config/express.js
+++ config/express.js
@@ -6,6 +6,8 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set("json spaces",	 4);
+
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
